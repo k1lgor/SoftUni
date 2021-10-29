c507011e95ad0e6d88c102e8c6792413de659208
Plamen Ivanov <paco.iwanow@gmail.com>
2021-10-29 10:48:41 +0300

    update of code

--- Python Fundamentals/Objects and Classes/Exercise/vehicle.py
+++ Python Fundamentals/Objects and Classes/Exercise/vehicle.py
@@ -1,36 +1,36 @@
 class Vehicle:
-    owner = None
-    
-    def __init__(self, vehicle_type, model, price):
-        self.vehicle_type = vehicle_type
+    def __init__(self, type, model, price):
+        self.type = type
         self.model = model
         self.price = price
+        self.owner = None
         
-    def buy(self, money, owner):
-        if self.price > money:
-            return "Sorry, not enough money"
-        elif self.owner is not None:
+    def buy(self, money: int, owner: str):
+        if self.owner:
             return "Car already sold"
-        else:
+        elif money < self.price:
+            return "Sorry, not enough money"
+        elif money >= self.price:
             self.owner = owner
-            return f"Successfully bought a {self.vehicle_type}. Change: {(money - self.price):.2f}"
+            return f"Successfully bought a {self.type}. Change: {money - self.price:.2f}"
         
     def sell(self):
-        if self.owner is None:
+        if self.owner:
+            self.owner = None
+        else:
             return "Vehicle has no owner"
-        self.owner = None            
     
     def __repr__(self):
-        if self.owner is not None:
-            return f"{self.model} {self.vehicle_type} is owned by: {self.owner}"
-        return f"{self.model} {self.vehicle_type} is on sale: {self.price}"
-    
-vehicle_type = "car"
+        if self.owner != None:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        return f"{self.model} {self.type} is on sale: {self.price}"
+
+type = "car"
 model = "BMW"
 price = 30000
-vehicle = Vehicle(vehicle_type, model, price)
-print(vehicle.buy(15000, "Peter"))
-print(vehicle.buy(35000, "George"))
+vehicle = Vehicle(type, model, price)
+vehicle.buy(15000, "Peter")
+vehicle.buy(35000, "George")
 print(vehicle)
 vehicle.sell()
 print(vehicle)
