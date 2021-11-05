b1ab9de678d047ade736018ec1f4da95bae671a9
Plamen Ivanov <paco.iwanow@gmail.com>
2021-11-05 20:03:14 +0200

    minor update of code

--- Python Fundamentals/Dictionaries/More Exercise/courses.py
+++ Python Fundamentals/Dictionaries/More Exercise/courses.py
@@ -9,9 +9,7 @@ while 'end' not in command:
 
     command = input()
 
-sort_by_number = sorted(courses.items(), key=lambda kvp: len(kvp[1]), reverse=True)
-for k, v in sort_by_number:
+for k, v in sorted(courses.items(), key=lambda kvp: len(kvp[1]), reverse=True):
     print(f"{k}: {len(v)}")
-    sort_by_name = sorted(v, key=str)
-    for n in sort_by_name:
+    for n in sorted(v):
         print(f"-- {n}")
