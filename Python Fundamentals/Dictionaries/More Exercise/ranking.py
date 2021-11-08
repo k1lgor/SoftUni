809e8f06c77c17ac41eed868d77e70f11e34e558
Plamen Ivanov <paco.iwanow@gmail.com>
2021-11-08 18:41:49 +0200

    update of the code

--- Python Fundamentals/Dictionaries/More Exercise/ranking.py
+++ Python Fundamentals/Dictionaries/More Exercise/ranking.py
@@ -1,44 +1,45 @@
-command = input()
-all_contests = {}
+def passwd_checker(curr_validity, curr_lang, curr_passwd):
+    curr_validity[curr_lang] = curr_passwd
+    return curr_validity
 
-while command != "end of contests":
-    lang, password = command.split(":")
-    all_contests.update({lang: password})
-    command = input()
 
-all_submissions = {}
-command = input()
-
-is_item = False
-while command != "end of submissions":
-    lang, password, username, points = command.split("=>")
-    points = int(points)
-    if lang in all_contests and all_contests[lang] == password:
-        if username in all_submissions:
-            is_item = False
-            for i in all_submissions[username]:
-                if lang == i['name']:
-                    is_item = True
-                    i["points"] = max(i["points"], points)
-            if not is_item:
-                all_submissions[username].append({"name": lang, "points": points})
+def users(curr_results, curr_lang, curr_passwd, curr_username, curr_points, curr_validity):
+    if curr_lang in curr_validity and curr_passwd == curr_validity[curr_lang]:
+        if curr_username not in curr_results:
+            curr_results[curr_username] = {curr_lang: curr_points}
+        elif curr_lang not in curr_results[curr_username]:
+            curr_results[curr_username][curr_lang] = curr_points
         else:
-            all_submissions[username] = [{"name": lang, "points": points}]
-    command = input()
+            curr_results[curr_username][curr_lang] = max(curr_results[curr_username][curr_lang], curr_points)
+    return curr_results
 
-best_points = {}
-for student, courses in sorted(all_submissions.items(), key=lambda x: x[0], reverse=False):
-    sorted_courses = sorted(courses, key=lambda c: -c['points'])
-    total_points = sum(course['points'] for course in courses)
-    best_points[student] = total_points
 
-best_student = sorted(best_points.items(), key=lambda p: -p[1])
+data = input()
+validity = {}
 
-name, best_points = best_student[0]
-print(f"Best candidate is {name} with total {best_points} points.")
-print("Ranking:")
+while data != 'end of contests':
+    lang, passwd = data.split(":")
+    validity = passwd_checker(validity, lang, passwd)
+    data = input()
 
-for student, courses in sorted(all_submissions.items(), key=lambda x: x[0], reverse=False):
-    print(student)
-    for course in sorted(courses, key=lambda z: -z['points']):
-        print(f"#  {course['name']} -> {course['points']}")
+data = input()
+results = {}
+
+while data != 'end of submissions':
+    lang, passwd, username, points = data.split("=>")
+    points = int(points)
+    results = users(results, lang, passwd, username, points, validity)
+
+    data = input()
+
+best_result = {}
+for k, v in results.items():
+    best_result[k] = 0
+    for x, z in v.items():
+        best_result[k] += z
+max_ = max(best_result.items(), key=lambda kvp: kvp[1])
+print(f"Best candidate is {max_[0]} with total {max_[1]} points.\nRanking:")
+for k, v in sorted(results.items(), key=lambda kvp: kvp[0]):
+    print(k)
+    for x, z in sorted(v.items(), key=lambda kvp: -kvp[1]):
+        print(f"#  {x} -> {z}")
