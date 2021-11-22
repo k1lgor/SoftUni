fb01dda7ddecca789de633e1e76a8720f0a67434
Plamen Ivanov <paco.iwanow@gmail.com>
2021-11-22 12:26:57 +0200

    upload

---
+++ Python Fundamentals/Regular Expressions/More Exercise/race.py
@@ -0,0 +1,20 @@
+import re
+
+names = [x for x in input().split(", ")]
+command = input()
+regex_names = {}
+
+while 'end of race' not in command:
+    name = ''.join([x.group() for x in re.finditer(r"([a-zA-Z]+)", command)])
+    dist = sum((int(x.group()) for x in re.finditer(r"\d", command)))
+    if name in names:
+        if name in regex_names:
+            regex_names[name] += dist
+        else:
+            regex_names[name] = dist
+
+    command = input()
+places = {}
+winners = [k for k, v in sorted(regex_names.items(), key=lambda x: -(x[1]))]
+print(
+    f"1st place: {winners[0]}\n2nd place: {winners[1]}\n3rd place: {winners[2]}")
