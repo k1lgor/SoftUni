17161ed3960d5a8a79b36d61d8f075e918558a69
Plamen Ivanov <paco.iwanow@gmail.com>
2021-11-22 13:46:43 +0200

    upload

---
+++ Python Fundamentals/Regular Expressions/More Exercise/softuni_bar_income.py
@@ -0,0 +1,16 @@
+import re
+
+command = input()
+total = 0
+
+while 'end of shift' not in command:
+    regex = re.findall(
+        r"%(?P<customer>[A-Z][a-z]+)%[^|$%.]*<(?P<product>\w+)>[^|$%.]*\|(?P<count>\d+)\|[^\d|$%.]*(?P<price>\d+\.?\d+)\$", command)
+    if regex:
+        print(
+            f"{regex[0][0]}: {regex[0][1]} - {int(regex[0][2]) * float(regex[0][3]):.2f}")
+        total += int(regex[0][2]) * float(regex[0][3])
+
+    command = input()
+
+print(f"Total income: {total:.2f}")
