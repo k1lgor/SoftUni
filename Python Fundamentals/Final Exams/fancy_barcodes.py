6752a34d99565260e6a3b279e5dc16cba16c7c7b
Plamen Ivanov <paco.iwanow@gmail.com>
2021-11-27 17:38:58 +0200

    upload

---
+++ Python Fundamentals/Final Exams/fancy_barcodes.py
@@ -0,0 +1,19 @@
+import re
+
+number = int(input())
+
+for _ in range(number):
+    barcode = re.match(
+        r"(@#+)([A-Z][A-Za-z0-9]{4,}[A-Z])(@#+)", input())
+
+    product_group = ''
+
+    if barcode:
+        for i in barcode.group(2):
+            if i.isdigit():
+                product_group += i
+        if product_group == '':
+            product_group = '00'
+        print(f"Product group: {product_group}")
+    else:
+        print("Invalid barcode")
