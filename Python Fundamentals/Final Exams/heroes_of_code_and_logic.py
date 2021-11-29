6a3127359a1791bce7f98176566ff3f871a950e3
Plamen Ivanov <paco.iwanow@gmail.com>
2021-11-29 19:25:56 +0200

    functions added

--- Python Fundamentals/Final Exams/heroes_of_code_and_logic.py
+++ Python Fundamentals/Final Exams/heroes_of_code_and_logic.py
@@ -1,3 +1,46 @@
+def cast(h, tok0, tok1, tok2):
+    if h[tok0]['mp'] >= int(tok1):
+        h[tok0]['mp'] -= int(tok1)
+        print(
+            f"{tok0} has successfully cast {tok2} and now has {h[tok0]['mp']} MP!")
+    else:
+        print(f"{tok0} does not have enough MP to cast {tok2}!")
+    return h
+
+
+def damage(h, tok0, tok1, tok2):
+    h[tok0]['hp'] -= int(tok1)
+    if h[tok0]['hp'] > 0:
+        print(
+            f"{tok0} was hit for {tok1} HP by {tok2} and now has {h[tok0]['hp']} HP left!")
+    else:
+        print(f"{tok0} has been killed by {tok2}!")
+        del h[tok0]
+    return h
+
+
+def recharge(h, tok0, tok1):
+    h[tok0]['mp'] += int(tok1)
+    if h[tok0]['mp'] < 200:
+        recovered = int(tok1)
+    else:
+        recovered = 200 - h[tok0]['mp'] + int(tok1)
+        h[tok0]['mp'] = 200
+    print(f"{tok0} recharged for {recovered} MP!")
+    return h
+
+
+def heal(h, tok0, tok1):
+    h[tok0]['hp'] += int(tok1)
+    if h[tok0]['hp'] < 100:
+        recovered = int(tok1)
+    else:
+        recovered = 100 - h[tok0]['hp'] + int(tok1)
+        h[tok0]['hp'] = 100
+    print(f"{tok0} healed for {recovered} HP!")
+    return h
+
+
 number = int(input())
 hero = {}
 
@@ -11,36 +54,17 @@ while 'End' not in command:
     command, *tokens = command.split(" - ")
 
     if command == 'CastSpell':
-        if hero[tokens[0]]['mp'] >= int(tokens[1]):
-            hero[tokens[0]]['mp'] -= int(tokens[1])
-            print(
-                f"{tokens[0]} has successfully cast {tokens[2]} and now has {hero[tokens[0]]['mp']} MP!")
-        else:
-            print(f"{tokens[0]} does not have enough MP to cast {tokens[2]}!")
+        cast(hero, tokens[0], tokens[1], tokens[2])
+
     elif command == 'TakeDamage':
-        hero[tokens[0]]['hp'] -= int(tokens[1])
-        if hero[tokens[0]]['hp'] > 0:
-            print(
-                f"{tokens[0]} was hit for {tokens[1]} HP by {tokens[2]} and now has {hero[tokens[0]]['hp']} HP left!")
-        else:
-            print(f"{tokens[0]} has been killed by {tokens[2]}!")
-            del hero[tokens[0]]
+        damage(hero, tokens[0], tokens[1], tokens[2])
+
     elif command == 'Recharge':
-        hero[tokens[0]]['mp'] += int(tokens[1])
-        if hero[tokens[0]]['mp'] < 200:
-            recovered = int(tokens[1])
-        else:
-            recovered = 200 - hero[tokens[0]]['mp'] + int(tokens[1])
-            hero[tokens[0]]['mp'] = 200
-        print(f"{tokens[0]} recharged for {recovered} MP!")
+        recharge(hero, tokens[0], tokens[1])
+
     elif command == 'Heal':
-        hero[tokens[0]]['hp'] += int(tokens[1])
-        if hero[tokens[0]]['hp'] < 100:
-            recovered = int(tokens[1])
-        else:
-            recovered = 100 - hero[tokens[0]]['hp'] + int(tokens[1])
-            hero[tokens[0]]['hp'] = 100
-        print(f"{tokens[0]} healed for {recovered} HP!")
+        heal(hero, tokens[0], tokens[1])
+
     command = input()
 
 for k, v in sorted(hero.items(), key=lambda x: (-x[1]['hp'], x[0])):
