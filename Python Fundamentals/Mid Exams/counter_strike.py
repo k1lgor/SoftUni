da28efdd6a82addfaa25d46ea2ae58f94ae6300d
Plamen Ivanov <paco.iwanow@gmail.com>
2021-12-07 19:35:49 +0200

    update

--- Python Fundamentals/Mid Exams/counter_strike.py
+++ Python Fundamentals/Mid Exams/counter_strike.py
@@ -2,22 +2,21 @@ energy, distance = int(input()), input()
 won_battle = 0
 
 while True:
-    
+
     if won_battle % 3 == 0:
         energy += won_battle
-        
+
     if 'End of battle' in distance:
         print(f"Won battles: {won_battle}. Energy left: {energy}")
         break
-    
+
     if energy < int(distance):
-        if energy < 0:
-            energy = 0
-        print(f"Not enough energy! Game ends with {won_battle} won battles and {energy} energy")
+        energy = max(energy, 0)
+        print(
+            f"Not enough energy! Game ends with {won_battle} won battles and {energy} energy")
         break
     else:
         energy -= int(distance)
         won_battle += 1
-    
-    distance = input()
 
+    distance = input()
