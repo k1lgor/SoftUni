30d4f0f5afbad06a81973c4ef1de6640a8a61c9d
Plamen Ivanov <paco.iwanow@gmail.com>
2022-01-09 11:56:17 +0200

    update of code

--- Python Fundamentals/Text Processing/Others/count_substring_occurance.py
+++ Python Fundamentals/Text Processing/Others/count_substring_occurance.py
@@ -1,15 +1,17 @@
-string = input().lower()
-substring = input().lower()
-counter = 0
-start = 0
+def count(string, substring):
+    counter = 0
+    start = 0
 
-while start < len(string):
-    
-    pos = string.find(substring, start)
-    
-    if pos != -1:
-        start = pos + 1
-        counter += 1
-    else:
-        break
-print(counter)
+    while start < len(string):
+
+        pos = string.find(substring, start)
+
+        if pos != -1:
+            start = pos + 1
+            counter += 1
+        else:
+            break
+    print(counter)
+
+
+count(input().lower(), input().lower())
--- Python Fundamentals/Text Processing/Others/diamond_problem.py
+++ Python Fundamentals/Text Processing/Others/diamond_problem.py
@@ -1,28 +1,32 @@
-string = [x for x in input()]
-no_more = False
+def diamond(string):
 
-while string != []:
-    for i in string[::]:
-        if string == [] and diamond != 0:
+    no_more = False
+
+    while string != []:
+        for i in string[::]:
+            if string == [] and diamond != 0:
+                break
+            if i == '>':
+                end = string.index(i)
+                string = string[end + 1:]
+                continue
+            diamond = 0
+            start = 0
+            end = 0
+            if i == '<':
+                start = string.index(i)
+                for j in string[start:]:
+                    if j == '>':
+                        end = string.index(j)
+                        diamond = sum(int(k)
+                                      for k in string[start:end] if k.isdigit())
+                        print(f'Found {diamond} carat diamond')
+                        string = string[end + 1:]
+                        break
+                break
+        if diamond == 0:
+            print('Better luck next time')
             break
-        if i == '>':
-            end = string.index(i)
-            string = string[end + 1:]
-            continue
-        diamond = 0
-        start = 0
-        end = 0
-        if i == '<':
-            start = string.index(i)
-            for j in string[start:]:
-                if j == '>':
-                    end = string.index(j)
-                    diamond = sum(int(k)
-                                  for k in string[start:end] if k.isdigit())
-                    print(f'Found {diamond} carat diamond')
-                    string = string[end + 1:]
-                    break
-            break
-    if diamond == 0:
-        print('Better luck next time')
-        break
+
+
+diamond(list(input()))
