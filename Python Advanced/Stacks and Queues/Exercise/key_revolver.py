e545ea1bc63e059183903fbee29b4f280d5257f7
Plamen Ivanov <paco.iwanow@gmail.com>
2022-01-25 22:08:19 +0200

    upload

---
+++ Python Advanced/Stacks and Queues/Exercise/key_revolver.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+bullet_price = int(input())
+barrel = int(input())
+bullets = [int(x) for x in input().split(' ')]
+locks = deque([int(x) for x in input().split(' ')])
+intelligence = int(input())
+
+ttl_bullets = 0
+counter = 0
+
+while True:
+    if not bullets or len(locks) == 0:
+        break
+    if bullets.pop() > locks[0]:
+        print("Ping!")
+    else:
+        locks.popleft()
+        print("Bang!")
+    ttl_bullets += 1
+    counter += 1
+    if counter % barrel == 0 and bullets:
+        print("Reloading!")
+
+if len(locks) > len(bullets):
+    print(f"Couldn't get through. Locks left: {len(locks)}")
+else:
+    print(f"{len(bullets)} bullets left. Earned ${intelligence - (ttl_bullets * bullet_price)}")
