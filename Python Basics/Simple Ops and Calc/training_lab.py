a7995ee0ac347de3275d570286e914b0fa3c1511
Plamen Ivanov <paco.iwanow@gmail.com>
2022-01-26 14:21:35 +0200

    one liner

--- Python Basics/Simple Ops and Calc/training_lab.py
+++ Python Basics/Simple Ops and Calc/training_lab.py
@@ -1,5 +1,6 @@
-w, h = float(input()), float(input())
-
-c = (h - 1) // 0.7
-r = w // 1.2
-print(c * r - 3)
+# w, h = float(input()), float(input())
+# 
+# c = (h - 1) // 0.7
+# r = w // 1.2
+# print(c * r - 3)
+print((float(input()) // 1.2) * ((float(input()) - 1) // 0.7) - 3)
