f203a5a4820651bf8d0a57d9c2494ab268ad1057
Plamen Ivanov <paco.iwanow@gmail.com>
2022-01-27 18:17:49 +0200

    upload

---
+++ Python Advanced/More Exercises/milkshakes.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+
+chocolates = [int(x) for x in input().split(', ')]
+milk_cups = deque(int(x) for x in input().split(', '))
+
+milkshakes = 0
+
+while chocolates and milk_cups and milkshakes < 5:
+
+    if chocolates[-1] > 0 and milk_cups[0] > 0:
+        if chocolates[-1] == milk_cups[0]:
+            chocolates.pop()
+            milk_cups.popleft()
+            milkshakes += 1
+        else:
+            milk_cups.append(milk_cups.popleft())
+            chocolates[-1] -= 5
+    elif chocolates[-1] <= 0 and milk_cups[0] <= 0:
+        chocolates.pop()
+        milk_cups.popleft()
+        continue
+    elif chocolates[-1] <= 0:
+        chocolates.pop()
+        continue
+    elif milk_cups[0] <= 0:
+        milk_cups.popleft()
+        continue
+
+
+if milkshakes == 5:
+    print("Great! You made all the chocolate milkshakes needed!")
+else:
+    print("Not enough milkshakes.")
+if chocolates:
+    print(f"Chocolate: {', '.join(str(x) for x in chocolates)}")
+else:
+    print("Chocolate: empty")
+if milk_cups:
+    print(f"Milk: {', '.join(str(x) for x in milk_cups)}")
+else:
+    print("Milk: empty")
