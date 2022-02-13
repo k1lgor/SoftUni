a5fd7269787e57a888531e9ec1cfe42d3f0c9d90
Plamen Ivanov <65338982+k1lgor@users.noreply.github.com>
2022-02-13 14:30:30 +0200

    Delete Python Advanced/Funcation Advanced/Lab/Exercise directory

--- Python Advanced/Funcation Advanced/Lab/Exercise/concatecate.py
+++
@@ -1,6 +0,0 @@
-def concatenate(*args):
-    return ''.join(args)
-
-
-print(concatenate("Soft", "Uni", "Is", "Great", "!"))
-print(concatenate("I", " ", "Love", " ", "Python"))
--- Python Advanced/Funcation Advanced/Lab/Exercise/even_or_odd.py
+++
@@ -1,9 +0,0 @@
-def even_odd(*args):
-    if args[-1] == 'even':
-        return [args[x] for x in range(len(args) - 1) if not args[x] % 2]
-    else:
-        return [args[x] for x in range(len(args) - 1) if args[x] % 2]
-
-
-print(even_odd(1, 2, 3, 4, 5, 6, "even"))
-print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
