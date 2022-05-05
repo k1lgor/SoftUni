1092c046e3f9575d24eb37be50acba1fce59e1a2
Plamen Ivanov <paco.iwanow@gmail.com>
2022-05-05 16:51:35 +0300

    add inheritance

---
+++ Python OOP/Inheritance/Exercise/zoo/main.py
@@ -0,0 +1,14 @@
+from project.mammal import Mammal
+from project.animal import Animal
+from project.bear import Bear
+from project.gorilla import Gorilla
+from project.lizard import Lizard
+from project.reptile import Reptile
+from project.snake import Snake
+
+mammal = Mammal("Stella")
+print(mammal.__class__.__bases__[0].__name__)
+print(mammal.name)
+lizard = Lizard("John")
+print(lizard.__class__.__bases__[0].__name__)
+print(lizard.name)
