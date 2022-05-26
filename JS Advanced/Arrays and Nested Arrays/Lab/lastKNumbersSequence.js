f390a3d0c2f26e61c62a7f8a369a0840bc73da21
Plamen Ivanov <paco.iwanow@gmail.com>
2022-05-26 17:14:28 +0300

    refactor

--- JS Advanced/Arrays and Nested Arrays/Lab/lastKNumbersSequence.js
+++ JS Advanced/Arrays and Nested Arrays/Lab/lastKNumbersSequence.js
@@ -1,7 +1,9 @@
 function lastK(totalLength, lastNumbers) {
-  let lastNum = 1
-  let arr = []
-  for (let i = 0; i < totalLength; i++) {
-    
-  }
+	let arr = [1];
+	for (let i = 0; i < totalLength - 1; i++) {
+		arr.push(arr.slice(-lastNumbers).reduce((a, b) => a + b));
+	}
+	return arr;
 }
+console.log(lastK(6, 3));
+console.log(lastK(8, 2));
